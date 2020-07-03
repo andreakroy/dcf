933d0f8f862e4a4eeb0ffd1d877f5693c8020100
Andrea Roy <akroy2@illinois.edu>
2020-07-02 17:13:37 -0700

    implemented ebit calculation

--- dcf.py
+++ dcf.py
@@ -1,13 +1,13 @@
 import requests
 import argparse as ap
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import yfinance as yf
 import json
-import pandas as pd
-
+import re
 class stock:
     def __init__(self, ticker: str, cik: int, name: str, sector: str, 
-            last_10k_date: str, accession_num: str, beta: float):
+            last_10k_date: str, accession_num: str,  beta: float,
+            net_income: int, tax_expense: int, interest_expense: int):
         self.ticker = ticker
         self.cik = cik
         self.name = name
@@ -15,6 +15,9 @@ class stock:
         self.last_10k_date = last_10k_date
         self.accession_num = accession_num
         self.beta = beta
+        self.net_income = net_income
+        self.tax_expense = tax_expense
+        self.interest_expense = interest_expense
     
     # using CAPM formuala:
     # Risk Free Rate + [Beta x ( Expected Market Return - Risk Free Rate )]
@@ -26,16 +29,22 @@ class stock:
    
     # using the formula EBIT / Interest Expenses
     def interest_coverage_ratio(self):
-        return 0
+        return self.ebit() / self.interest_expense
+    
+    # using tables from A. Damodaran to relate interest coverage ratio to
+    # default spread.
+    def default_spread(self):
+        ic = self.interest_coverage_ratio
+
+
     
     # calculation of Earnings Before Interest and Taxes
-    # using EBIT = Revenue - Cost of Goods and Services - OPEX
-    #            = Gross Profit - OPEX
-    # Pulling Gross Profit and OPEX data from latest annual report in Edgar
-    def ebit(self):
-        return 0
+    # using EBIT = Net Income + Interest Expense + Tax Expense
+    def ebit(self)
+        return self.net_income + self.tax_expense + self.interest_expense
+
     def __str__(self):
-        return "{} ({} - CIK: {})\nSector: {}\nLast Annual Report ({}): {}".format(
+        return '{} ({} - CIK: {})\nSector: {}\nLast 10-K ({}): {}'.format(
                 self.name, self.ticker, self.cik, self.sector,
                 self.accession_num, self.last_10k_date)
 
@@ -50,31 +59,55 @@ def get_company_data(ticker) -> stock:
     if xml.status_code != requests.codes.ok:
         return None
     page = BeautifulSoup(xml.text, 'lxml')
-    try:
-        # pull data from Yahoo Finance API
-        t = yf.Ticker(ticker)
-        info = t.info
-        
-        # find latest annual report (SEC 10-K)
-        # assumes that the latest annual report is the first in the list
-        # returned
-        report_entry = page.find('entry')
-        #report = requests.get(report_entry.find('filing-href').text).text
-        #rep = BeautifulSoup(report, 'lxml')
-        print(report_entry.find('accession-nunber').text)
-        #print(report.text)
-        return stock(
-            ticker.upper(),
-            int(page.cik.text),
-            info['longName'], 
-            page.find('assigned-sic-desc').text,
-            report_entry.find('filing-date').text,
-            # "accession-nunmber" [sic] is the tag in the xml schema
-            report_entry.find('accession-nunber').text,
-            float(info['beta'])
-        )
-    except:
-        return None
+    
+    # pull data from Yahoo Finance API
+    t = yf.Ticker(ticker)
+    info = t.info
+    
+    # find the accession number latest annual report (SEC 10-K)
+    # assumes that the latest annual report is the first in the list
+    # returned
+    report_entry = page.find('entry')
+    # Why the heck is "accession-nunmber" the tag in the xml schema???
+    acc_num = ''.join(filter(lambda x: x != '-', 
+        report_entry.find('accession-nunber').text))
+    # Gets the company CIK
+    cik = int(page.cik.text)
+    
+    '''
+    # Parses the xml to find the id of the doc with income or operations
+    # statements
+    xml = requests.get('https://sec.gov/cgi-bin/viewer',
+            params={'action': 'view', 'cik': cik,
+                'accession_number': acc_num})
+    report = BeautifulSoup(xml.text, 'lxml')
+    doc_id = report.find(lambda tag: re.compile(
+        '(?i)consolidated statements of income').match(tag.text))['id']
+    
+    xml = requests.get('https://sec.gov/Archives/edgar/data/{}/{}'.format(
+        cik, acc_num))
+    docs = BeautifulSoup(xml.text, 'lxml')
+    link = docs.find(lambda tag: re.compile(
+        '(?i){}\.'.format(doc_id)).match(tag.text)).find('a')['href']
+    print(link)
+    xml = requests.get('https://sec.gov{}'.format(link))
+    income_sheet = BeautifulSoup(xml.text, 'lxml')
+    net_income = income_sheet.find(lambda tag: re.compile(
+        '(?i)net').search(tag.text))
+    print(net_income)
+    '''
+    return stock(
+        ticker.upper(),
+        cik,
+        info['longName'], 
+        page.find('assigned-sic-desc').text,
+        report_entry.find('filing-date').text,
+        acc_num,
+        float(info['beta']),
+        0,
+        0,
+        0
+    )
 
 # Gets the current Risk Free Rate (the 10 Year US Treasury
 # Bond yield) by querying the US Treasury website.
@@ -88,5 +121,5 @@ def get_risk_free_rate() -> float:
 def wacc(re, rd, e, d, t, v):
     return 0
 
-msft = get_company_data("aal")
+msft = get_company_data("goog")
 print(msft)
